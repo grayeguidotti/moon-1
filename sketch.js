362aae9bb6650dc967cd8e1d33800b7a678cf72a
Your Name <you@example.com>
2026-01-20 11:27:11 -0800

    added push pop

--- sketch.js
+++ sketch.js
@@ -9,14 +9,19 @@ function draw() {
 
 function drawTarget(x, y, size){
   noStroke();
-
+  push();
   fill(0, 0, 255);
   circle(x, y, size * 0.5);
+  pop();
 
+  push();  
   fill(255,0, 0);
   circle(x, y, size * 0.75);
+  pop();
 
+  push();
   fill(255);
   circle(x, y, size);
+  pop();
 
 }
