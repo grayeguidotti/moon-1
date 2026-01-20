3e2a40ed306e0a4659b72d4051017847de3eb4d2
Your Name <you@example.com>
2026-01-20 11:28:48 -0800

    fixed colors

--- sketch.js
+++ sketch.js
@@ -10,17 +10,17 @@ function draw() {
 function drawTarget(x, y, size){
   noStroke();
   push();
-  fill(0, 0, 255);
+  fill(255, 0, 0);
   circle(x, y, size * 0.5);
   pop();
 
   push();  
-  fill(255,0, 0);
+  fill(255);
   circle(x, y, size * 0.75);
   pop();
 
   push();
-  fill(255);
+  fill(0, 0, 255);
   circle(x, y, size);
   pop();
 
