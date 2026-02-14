9bc3b57d2a138eb1831832cfffcab66937499ff1
Your Name <you@example.com>
2026-02-13 20:29:14 -0800

    draw moon and shadow, renamed page in html file

--- sketch.js
+++ sketch.js
@@ -5,26 +5,27 @@ function preload(){
 }     
 
 function setup() {                            // a sketch that has a drawMoon function, uses phase (0, 30) using an image of a moon and a black ellipse moving in front of the image to give waxing and waning moon)
-  createCanvas(fullscreen);
+  createCanvas(800, 800);
   
 
 }
 
 function draw() {
   background(0);
+  // push();
   imageMode(CENTER);
-  image(img);                  //display the image
-  describe('an image of a pixelated moon');                 
-  // drawMoonShadow(width/2, height/2, 150);
+  image(img, width/2, height/2, 600, 600);                  //draw the image, in the center 500 pixels big
+  describe('an image of a pixelated moon');
+  // pop();            
+
+  drawMoonShadow(width/2, height/2, 500);                 //draw black "shadow"" 500 pixels big to cover moon image
 }
 
-// function drawMoonShadow(x, y, size){
-//   push();                                       // draw black "shadow""
-//   stroke(255)
-//   fill(0);
-//   strokeWeight(1);
-//   circle(x, y, size);
-//   pop();
-// }
+function drawMoonShadow(x, y, size){                                       // 
+  stroke(255)
+  fill(0);
+  strokeWeight(1);
+  circle(x, y, size);
+}
 
-//move black shadow over 
+   //move black shadow over 
